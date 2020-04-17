f38cf5b2065ea7b2d0e7e284c3b28a37bd4add39
jirwincline <jci@pdx.edu>
2020-04-16 18:27:07 -0700

    created an inut to search for coins by name

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -1,7 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import SearchResult from "./SearchResult";
 
-export default class Search extends Component {
-  render() {
-    return <div>search</div>;
-  }
-}
+const Search = (props) => {
+  const results = props.searchResults.map((curr) => {
+    return (
+      <SearchResult
+        key={curr.id}
+        currency={curr}
+        handleSelect={props.handleSelect.bind(null, curr)}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <h1>Cryptocurrency Portfolio Calculator</h1>
+      <form>
+        <div className="form-group">
+          <label>Search for a Currency:</label>
+          <br />
+          <input
+            onChange={props.handleChange}
+            autoComplete="off"
+            type="text"
+            name="name"
+            placeholder="Ex: Bitcoin, Litecoin, Ethereum..."
+            value={props.name}
+            className="field"
+          />
+        </div>
+        <div className="currency-list">{results}</div>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
