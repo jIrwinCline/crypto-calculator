7c1580ffb60dc30f25689a1bb86a1fc5e5c9744a
jirwincline <jci@pdx.edu>
2020-04-16 17:53:50 -0700

    add index.jsx

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import PortfolioContainer from './PortfolioContainer'
-import axios from 'axios'
+import axios from "axios";
 
-const csrfToken = document.querySelector('[name="csrf-token"]').content
-axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+const csrfToken = document.querySelector('[name="csrf-token"]').content;
+axios.defaults.headers.common["X-CSRF-TOKEN"] = csrfToken;
 
 class App extends Component {
-  render(){
-    return(
-    //   <PortfolioContainer/>
-    )
+  render() {
+    return (
+      <div>Hello world</div>
+      //   <PortfolioContainer/>
+    );
   }
 }
 
-export default App
+export default App;
